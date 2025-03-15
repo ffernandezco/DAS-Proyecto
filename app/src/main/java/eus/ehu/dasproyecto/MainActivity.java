9b7c12f67f62416066cc51fcaa0cae31e15dd38f
Francisco Fernández <ffernandez032@ikasle.ehu.eus>
2025-03-15 17:46:40 +0100

    Logotipo actualizado y colores

--- app/src/main/java/eus/ehu/dasproyecto/MainActivity.java
+++ app/src/main/java/eus/ehu/dasproyecto/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -44,8 +45,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         applyLanguageFromPreferences();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.primary_dark));
 
         // Setup permission request launcher
         requestPermissionLauncher = registerForActivityResult(
@@ -70,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         // Configura Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
 
         // DrawerLayout y NavigationView
         drawerLayout = findViewById(R.id.drawer_layout);
